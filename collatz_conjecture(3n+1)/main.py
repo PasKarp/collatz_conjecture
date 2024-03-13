735f1179a0204101d6c9823d0d45e338f6392673
Pascal <161959914+PasKarp@users.noreply.github.com>
2024-03-13 14:33:29 +0100

    Update main.py

--- collatz_conjecture(3n+1)/main.py
+++ collatz_conjecture(3n+1)/main.py
@@ -1,7 +1,13 @@
-import time
-
-
 def calc(n):
+    """
+    Calculate the next number in the Collatz sequence based on the given number.
+
+    Parameters:
+    - n (int): The current number in the sequence.
+
+    Returns:
+    - int: The next number in the sequence.
+    """
     if n % 2 == 0:
         n = int(n / 2)
         return n
@@ -11,6 +17,16 @@ def calc(n):
 
 
 def update_sequence_dict(seq_dict, current_number):
+    """
+    Update the given dictionary with Collatz sequence information starting from the specified number.
+
+    Parameters:
+    - seq_dict (dict): Dictionary to store Collatz sequence information.
+    - current_number (int): The starting number for the sequence update.
+
+    Returns:
+    - None
+    """
     while current_number not in seq_dict:
         next_number = calc(current_number)
         seq_dict[current_number] = next_number
@@ -19,11 +35,9 @@ def update_sequence_dict(seq_dict, current_number):
 
 sequence_dict = {}
 
-start = time.time()
+# Generate and update Collatz sequences for numbers from 1 to 10000
 for i in range(1, 10001):
     update_sequence_dict(sequence_dict, i)
 
-end = time.time()
-
+# Print the resulting Collatz sequence dictionary
 print(sequence_dict)
-print({"Time taken": end - start})
